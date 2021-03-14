90e18979473e210f812d801b89ec11ccc3e768a4
wuxue <wuxue@windimg.com>
2021-03-14 14:32:30 +0800

    vue

--- vue/index.js
+++ vue/index.js
@@ -23,3 +23,38 @@
 
   // Vue 在修改数据后，视图不会立刻更新，而是等同一事件循环中的所有数据变化完成之后，再统一进行视图更新。  nextTick
   // 在下一次DOM更新循环结束之后执行延迟回调  nextTick
+
+  // v-html v-show v-bind v-on v-model
+  // v-if v-else v-text（主要是更新textContent）
+
+  // v-show display: none/block，高初始渲染开销， 频繁切换可使用
+  // v-if 是否渲染dom
+
+  // 父组件props传值   子组件$emit传值
+
+  // watch 中 deep表示深层遍历，监听对象所有属性变换
+
+  // this.$set(this.obj, b, 'obj.b')  $set手动将数据处理为响应式
+
+  // delete 和Vue.delete区别  delete将删除元素变为empty/undefined，其他元素键值还是不变   Vue.delete 直接删除数据，改变键值
+
+  //computed和watch的区别
+    // computed 支持缓存，只有依赖数据改变，才会重新计算， 不支持异步
+    // watch支持异步， 当一个属性发生变换时，需执行对应操作；一对多
+
+//生命周期
+    //beforeCreate 
+
+//vue双向绑定原理
+    //采用数据劫持结合发布者-订阅者模式，通过Object.defineProperty()来劫持各个属性的setter、getter,在数据变动时发布消息给订阅者。触发监听回调
+
+// vue的响应式原理从object.defineProperty变为Proxy有什么好处
+    // object.defineProprety 只能劫持对象属性，需对每个对象的每个属性进行遍历   proxy劫持整个对象，并返回一个新对象，不仅可以代理对象，也可以代理数组，还可代理动态新增的属性
+
+// vue组件data为什么必须是函数
+    // 组件可复用，js中对象存在引用关系，如果组件data是对象，那么子组件data值会相互污染，产生副作用
+
+//网页从输入网址到渲染完成那些过程
+    // 
+
+
