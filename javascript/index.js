53ef20acedb42ae2ff60482fafc4962dcd287bb4
wuxue <wuxue@windimg.com>
2021-03-15 12:27:52 +0800

    js

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+// 只有函数有作用域 if没有作用域
+    
+
+// object.prototype.toString.call
+
+// 宏任务  script  setTimeout setInterval setImmediate
+// 先执行微任务，再执行宏任务
+
+// js数据类型
+    // js基本数据类型   undefined null string number Boolean es6(Symbnol)独一无二不可变更类型
+    // 引用数据类型  object function date Regex Array
+
+// jsonp为什么可以实现跨域
+    // 
